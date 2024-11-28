866159f5bce1a9106e7af569bfe3b2e92b89ca2b
ckduswn <nkcd_1@naver.com>
2024-11-28 17:17:26 +0900

    Add files via upload

--- Python Basic/project_collector/fnc_service.py
+++ Python Basic/project_collector/fnc_service.py
@@ -7,12 +7,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
 # 디폴트 : SELENIUM 코드 종료 → 웹브라우저 종료
 # 1. 셀레니움 웹브라우저 설정
 options = Options()
-options.add_experimental_option("detach", True) # 웹 브라우저 종료 X, 개발 완료 시 삭제.
-# options.add_argument("headless") # 셀레니움 백그라운드 동작(개발 완료 시 사용. 지금은 작동하는지 계속 확인해야 하니까 사용하디망.)
+# options.add_experimental_option("detach", True) # 웹 브라우저 종료 X, 개발 완료 시 삭제.
+options.add_argument("headless") # 셀레니움 백그라운드 동작(개발 완료 시 사용. 지금은 작동하는지 계속 확인해야 하니까 사용하디망.)
 # ※ 셀레니움이 로봇이 아닌 척 하는 방법(로봇인 거 걸리면 페이지가 수집 거부하는 경우 있음.)
 options.add_argument("disable-blink-features=AutomationContralled")
 options.add_experimental_option("useAutomationExtension", False)
@@ -33,18 +34,38 @@ def collect_news():
     # 1. collecting next art URL and url_list's URL 
     # 2. complete to collection 1 art → save that art URL on url_list
 
+    url_list=[] # 수집이 완료된 Link(URL) 저장
+    flag = False # while문 빠져나오기 위한 변수
+    news_list=[] # 
     while True:
     # 9건의 기사 수집
         doc = BeautifulSoup(driver.page_source, "html.parser")
         link_list = doc.select("article.content-article ul.list_newsheadline2 a.item_newsheadline2") 
     # shift + Tab : 앞으로 땡겨
         for link in link_list:
+            # 중복수집 체크
+            if link in url_list:
+                flag=True
+                break
             print(f"{count} ===================================")
-            get_news_info(link["href"])
+            news = get_news_info(link["href"]) 
+            news_list.append(news) # 수집한 데이터 로컬 저장
             count+=1
+            url_list.append(link) # 중복수집 방지를 위한 처리
+        if flag: #  if flag == True:를 이렇게 줄이도록 / 0은 False, 그외의 수는 다 True.
+            break
         # 뉴스 새로고침 버튼 클릭
         driver.find_element(By.XPATH,'').click()
         time.sleep(1)
+        
+        # 수집한 데이터 27건 main으로 전달
+        # 1. DB 활용
+        #   - DB(27건 저장) → SELECT 가져와서 전달
+        # 2. 수집시 수집데이터를 따로 저장 후 전달 news_list → [{news1}, {news2},...]
+        # "pandas"의 DataFrame(표) Type으로 news_list 변환
+        col_name = ["title", "writer", "content", "regdate"]
+        df_news=pd.DataFrame(news_list, columns=col_name) # DataFrame(표) 변환
+    return df_news, count
     
 def get_news_info(url:str):
     result = requests.get(url)
@@ -78,6 +99,6 @@ def get_news_info(url:str):
         "content" : content,
         "regdate" : reg_date,
     }
-    # insert_news(data) # DB에 안 넣을 땐 주석처리
-    
+    insert_news(data) # DB에 안 넣을 땐 주석처리 
+    return data
 collect_news()
--- Python Basic/project_collector/main.py
+++ Python Basic/project_collector/main.py
@@ -3,8 +3,22 @@
 # 2. 수집 : 데이터 수집(requests, beautifulsoup)
 # 3. 화면 : 출력, 엑셀 다운로드(streamlit, pandas)
 # 4. 저장 : 데이터베이스 저장(pymysql + MariaDB)
+
+# 데이터를 수집하는 이유?
+#  → 데이터 분석 or 인공지능 학습
+#  → CSV or JSON 수집
+# CSV  예 : 1, 최체리, 10, 여
+# JSON 예 :
+# {
+#     "번호":1,
+#     "이름":"최체리",
+#     "나이":10,
+#     "성별":여
+# }
+
 from fnc_service import collect_news
 import streamlit as st
+from datetime import datetime
 
 # Streamlit run project_collector/main.py
 # Streamlit docs → https://docs.streamlit.io/
@@ -19,14 +33,23 @@ def main():
     )
     st.title("NEWS :rainbow[COLLECTOR]")
     st.text("Boogie on and on.")
-    with st.form(key="form"):
-        submitted = st.form_submit_button("collect")
-
-    # 버튼 이벤트 → 수집 버튼을 클릭할 때
-    # 위에 submitted 변수 추가. 
-    if submitted:
-         collect_news()
-
+    if st.button("수집"):
+        df_news, count = collect_news()
+        st.write(f"뉴스 {count}건 수집 완료!")
+        st.write(df_news)
+        flag=True
+        # df_news : DataFrame → CSV type
+        news_csv=df_news.to_csv(index=False, encoding="utf8")
+    if flag:
+        now=datetime.now().strftime("%Y%m%d%H%M%S")
+        st.download_button(
+            Label="다운로드",
+            data=news_csv,
+            file_name=f"실시간뉴스_{now}.csv",
+            mime="text/csv",
+            key="download_csv"
+        )
+        
         # 유효성 체크 → 사용자가 입력한 값이 유효한 값인지 체크
         # 전처리(가공) → 유효한 값으로 변경
         # 1. 정규식을 활용한 유효성 체크
